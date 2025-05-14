568e9b32757794e3bd38074e22f4e389ad2b522e
Alex <st10437348@imconnect.edu.za>
2025-05-14 11:12:38 +0200

    Fixed white space errors

--- CyberChatBot/CyberChatBot/SecureBot.cs
+++ CyberChatBot/CyberChatBot/SecureBot.cs
@@ -2,13 +2,13 @@
 using System.Runtime.InteropServices;
 
 namespace CyberChatBot
-{ 
-    public class SecureBot : SecureBotBase
 {
-    public override void DisplayAsciiArt()
+    public class SecureBot : SecureBotBase
     {
-        Console.ForegroundColor = ConsoleColor.DarkMagenta;
-        Console.WriteLine(@"
+        public override void DisplayAsciiArt()
+        {
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+            Console.WriteLine(@"
 +------------------------------------------------------------+
 |                                                            |
 |    _____                            ____        _          |
@@ -20,89 +20,75 @@ namespace CyberChatBot
 |                                                            |
 +------------------------------------------------------------+
 ");
-        Console.ResetColor();
-    }
-
-    public override void PlayVoiceGreeting()
-    {
-        try
-        {
-            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
-            {
-                string filePath = "Audio/welcome.wav";
-                SoundPlayer player = new SoundPlayer(filePath);
-                player.Play();
-                Thread.Sleep(7000);
-            }
-            else
-            {
-                Console.WriteLine("Voice greeting is not supported on this platform.");
-            }
-        }
-        catch (Exception ex)
-        {
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine("Could not play voice greeting: " + ex.Message);
             Console.ResetColor();
         }
-    }
 
-    public override void PrintDivider()
-    {
-        Console.ForegroundColor = ConsoleColor.DarkGray;
-        Console.WriteLine("\n" + new string('-', 60));
-        Console.ResetColor();
-    }
-
-    public override void SimulateTyping(string message)
-    {
-        Console.WriteLine();
-        Console.ForegroundColor = ConsoleColor.Cyan;
-        Console.Write("SecureBot: ");
-        Console.ResetColor();
-
-        foreach (char c in message)
+        public override void PlayVoiceGreeting()
         {
-            Console.Write(c);
-            Thread.Sleep(30);
+            try
+            {
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                {
+                    string filePath = "Audio/welcome.wav";
+                    SoundPlayer player = new SoundPlayer(filePath);
+                    player.Play();
+                    Thread.Sleep(7000);
+                }
+                else
+                {
+                    Console.WriteLine("Voice greeting is not supported on this platform.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Could not play voice greeting: " + ex.Message);
+                Console.ResetColor();
+            }
         }
-        Console.WriteLine();
-    }
 
-    public void AskIfNeedAnythingElse()
-    {
-        string[] responses = {
+        public override void PrintDivider()
+        {
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.WriteLine("\n" + new string('-', 60));
+            Console.ResetColor();
+        }
+
+        public override void SimulateTyping(string message)
+        {
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.Write("SecureBot: ");
+            Console.ResetColor();
+
+            foreach (char c in message)
+            {
+                Console.Write(c);
+                Thread.Sleep(30);
+            }
+            Console.WriteLine();
+        }
+
+        public void AskIfNeedAnythingElse()
+        {
+            string[] responses = {
                 "Is there anything else you'd like to know?",
                 "Can I assist you with anything further?",
                 "Would you like help with something else?",
                 "Is there another question I can answer for you?"
             };
-        SimulateTyping(responses[new Random().Next(responses.Length)]);
-    }
+            SimulateTyping(responses[new Random().Next(responses.Length)]);
+        }
 
-    public void AskHowAreYou()
-    {
-        string[] responses = {
+        public void AskHowAreYou()
+        {
+            string[] responses = {
                 "I'm always ready and running smoothly, thanks for asking!",
                 "I'm functioning perfectly, ready to assist you!",
                 "All systems are go, thanks for asking!",
                 "I'm doing great, thank you for checking in!"
             };
-        SimulateTyping(responses[new Random().Next(responses.Length)]);
+            SimulateTyping(responses[new Random().Next(responses.Length)]);
+        }
     }
 }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
